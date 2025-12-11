951709417b5de6c4d3e00c6b0912cff9786b7da7
jacopocutrignelli <jacopo.cutrignelli@edu.unifi.it>
2025-12-11 14:29:13 +0100

    pattern Observer e classe Resource Loader che legge i file

--- main.cpp
+++ main.cpp
@@ -58,10 +58,78 @@ public:
                 // Calcola il progresso
                 float progress = static_cast<float>(loadedSize) / static_cast<float>(totalSize);
                 notify(progress, path);
-
-                // Simula tempo di caricamento (solo per effetti visivi)
                 std::this_thread::sleep_for(std::chrono::milliseconds(1));
             }
         }
     }
 };
+
+class ProgressBar : public IObserver {
+    sf::RectangleShape background;
+    sf::RectangleShape bar;
+    sf::Font font;
+    sf::Text percentText;
+    sf::Text fileText;
+
+    float progress = 0.f;
+    std::string currentFile;
+
+public:
+    ProgressBar(float width, float height)
+        : percentText(font, "", 20),
+          fileText(font, "", 18)
+    {
+        background.setSize({width, height});
+        background.setFillColor(sf::Color(50, 50, 50));
+        background.setOutlineThickness(2.f);
+        background.setOutlineColor(sf::Color::White);
+
+        bar.setSize({0.f, height});
+        bar.setFillColor(sf::Color::Green);
+
+
+        font.openFromFile("/System/Library/Fonts/Supplemental/Arial.ttf");
+
+        percentText.setFillColor(sf::Color::White);
+        fileText.setFillColor(sf::Color(180, 180, 180));
+    }
+
+    // Callback dell'observer: aggiorna barra e testi
+    void onProgressUpdate(float value, const std::string& file) override {
+        progress = value;
+        currentFile = file;
+
+        bar.setSize({
+            background.getSize().x * progress,
+            background.getSize().y
+        });
+
+        int percent = static_cast<int>(progress * 100);
+        percentText.setString(std::to_string(percent) + "%");
+
+        std::filesystem::path p(file);
+        fileText.setString("Loading: " + p.filename().string());
+    }
+
+    // Disegno della barra (sarà usato nel main il giorno 3)
+    void draw(sf::RenderWindow& window, sf::Vector2f pos) {
+        background.setPosition(pos);
+        bar.setPosition(pos);
+
+        percentText.setPosition({
+            pos.x + background.getSize().x / 2.f - 25.f,
+            pos.y - 30.f
+        });
+
+        fileText.setPosition({
+            pos.x,
+            pos.y + background.getSize().y + 10.f
+        });
+
+        window.draw(background);
+        window.draw(bar);
+        window.draw(percentText);
+        window.draw(fileText);
+    }
+};
+
